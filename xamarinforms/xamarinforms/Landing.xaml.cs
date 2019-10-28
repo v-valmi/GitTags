0a321a41364b46360f5e5c31fa32943984fb9108
ARAM Maliachi Diaz (DOP - Technology) <AR20064716@wipro.com>
2019-10-28 12:20:11 -0600

    testing android sdks in hosted mac-os latest

---
+++ xamarinforms/xamarinforms/Landing.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinforms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Landing : ContentPage
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+    }
+}
+// change in dev branch
